ff767e3e332acb7392d7f698632effa55252c815
Christos Angelopoulos <christakis2020@yandex.com>
2024-03-28 23:33:25 +0200

    setting nano as PREFERRED EDITOR

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ chmod +x wordy.sh&&cp wordy.sh ~/.local/bin/
 #Create the dir that will contain the
 mkdir -p ~/.local/share/wordy/ ~/.config/wordy/
 cp wordy.png ~/.local/share/wordy/
-echo -e  "WORD_LIST /usr/share/dict/words\nPREFERRED_EDITOR  nano">~/.config/wordy/wordy.config
+echo -e  "WORD_LIST /usr/share/dict/words\nPREFERRED_EDITOR  $EDITOR">~/.config/wordy/wordy.config
